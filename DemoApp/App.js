b7f4c1726e8df65c32f1c806fa0a0883b7f8ec7d
Rafal Ozog <rafalozog@gmail.com>
2024-01-04 16:06:30 +0000

    Dumb list implemented.

--- DemoApp/App.js
+++ DemoApp/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const listItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView style={styles.scrollView}>
+        {listItems.map((item, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text>{item}</Text>
+          </View>
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollView: {
+    width: '100%',
+  },
+  listItem: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
